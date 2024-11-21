2b162a3fbbb6f3a8b1fe0b07686adc4317c5512b
silvaernane <87151969+silvaernane@users.noreply.github.com>
2024-11-21 15:31:18 -0300

    Limpando arquivos para inicar o app notes

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,4 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "Hello World!";
-});
 
-Route::get('/about', function () {
-    echo "About";
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-Route::get('/page3/{value}', [MainController::class, 'page3']);
