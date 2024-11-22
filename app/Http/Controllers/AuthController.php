9808074453ec79a4cc012de780212bf5ad603131
silvaernane <87151969+silvaernane@users.noreply.github.com>
2024-11-22 16:15:37 -0300

    Adiciona validações no formulário de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,30 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text_username');
+        // form validation
+        $request->validate(
+            // rules
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            // error messages
+            [
+                'text_username.required' => 'O username é obrigatório!',
+                'text_username.email' => 'Username deve ser um e-mail válido!',
+                'text_password.required' => 'A password é obrigatória!',
+                'text_password.min' => 'Password deve ter no mínimo :min caracteres!',
+                'text_password.max' => 'Password deve ter no máximo :max caracteres!'
+
+            ]
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK';
+
     }
 
     public function logout()
